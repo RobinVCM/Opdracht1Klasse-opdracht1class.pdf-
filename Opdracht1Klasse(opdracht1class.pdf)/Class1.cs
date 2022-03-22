cd708990bdbc2ed97c719f7e3b5d7fb35c2b5b32
RobinVCM <robin.malfait@viso-cor-mariae.be>
2022-03-22 09:27:57 +0100

    Add project files.

---
+++ Opdracht1Klasse(opdracht1class.pdf)/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Leerling
+{
+    private string strVoornaam;
+    private string strNaam;
+    private string strKlas;
+
+    public string Voornaam
+    {
+        get { return strVoornaam; }
+        set { strVoornaam = value; }
+    }
+    public string Naam
+    {
+        get { return strNaam; }
+        set { strNaam = value; }
+    }
+    public string Klas
+    {
+        get { return strKlas; }
+        set { strKlas = value; }
+    }
+    public Leerling()
+    {
+        strVoornaam = "";
+        strNaam = "";
+        strKlas = "";
+    }
+    public static void GegevensWeergeven(Leerling lln)
+    {
+        Console.WriteLine("___________________________");
+        Console.WriteLine("\nNog eens overlopen:");
+        Console.WriteLine($"Voornaam: {lln.strVoornaam} ");
+        Console.WriteLine($"Naam: {lln.strNaam} ");
+        Console.WriteLine($"Klas: {lln.strKlas} ");
+
+    }
+
+    public static void AanvullenGegevens(Leerling lln)
+    {
+
+        Console.WriteLine("Voornaam:");
+        lln.strVoornaam = Convert.ToString(Console.ReadLine());
+        Console.WriteLine("Naam:");
+        lln.strNaam = Convert.ToString(Console.ReadLine());
+        Console.WriteLine("Klas:");
+        lln.strKlas = Convert.ToString(Console.ReadLine());
+    }
+
+}
---
+++ Opdracht1Klasse(opdracht1class.pdf)/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Opdracht1Klasse_opdracht1class.pdf_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Leerling lln = new Leerling();
+            Leerling.AanvullenGegevens(lln);
+            Leerling.GegevensWeergeven(lln);
+
+        }
+
+
+    }
+}
